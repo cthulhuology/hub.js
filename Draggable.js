c56f4fa35ef4c4e27a4e109973e50d00e9e74a92
David Goehrig <dgoehrig@critical-media.com>
2013-08-13 15:20:43 -0400

    adding draggable

---
+++ Draggable.js
@@ -0,0 +1,27 @@
+Draggable = function(element) {
+	var draggable =  function(method,x,y,button) {
+		switch(method) {
+		case 'down':
+			if  ( x < element.offsetLeft || x > (element.offsetLeft + element.offsetWidth) || y < element.offsetTop || y > (element.offsetTop + element.offsetHeight)) return;
+			element.dx = x - element.offsetLeft
+			element.dy = y - element.offsetTop
+			Hub('subscribe','move',this)
+			Hub('subscribe','up',this)
+			Hub('unsubscribe','down',this)
+			return this
+		case 'move':
+			element.style.left = (x - element.dx) + 'px'
+			element.style.top = (y - element.dy) + 'px'
+			return this;
+		case 'up':
+			Hub('subscribe','down',this)	
+			Hub('unsubscribe','move',this)	
+			Hub('unsubscribe','up',this)	
+			return this
+		default:
+			return this
+		}
+	}
+	Hub('subscribe','down',draggable)
+	return draggable
+}
